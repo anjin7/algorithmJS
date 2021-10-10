f5c241889428edc340823d926d2a19c9b7933967
AnJiIn <aji0719@gmail.com>
2021-10-10 20:50:56 +0900

    파일로 작성6

---
+++ bj_1712.js
@@ -0,0 +1,13 @@
+// let input = require('fs').readFileSync('/dev/stdin').toString().split(' ');
+
+let input = [1000, 70, 170];
+
+
+const A = input[0] * 1;
+const B = input[1] * 1;
+const C = input[2] * 1;
+
+const margin = C - B;
+const count = Math.floor(A / margin) + 1
+
+console.log(margin <= 0 ? -1 : count);
