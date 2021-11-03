378eeaf64e012a20e6b3a0d74616195ef32d89e8
AnJiIn <aji0719@gmail.com>
2021-11-04 00:44:17 +0900

    인프런2

---
+++ inf/inf_s105.js
@@ -0,0 +1,15 @@
+//최솟값 구하기
+
+function solution(arr){
+  let answer = Math.min(...arr);
+  return answer;
+}
+let arr = [5, 7, 2, 3, 2, 9, 11];
+//최솟값 구하기
+
+function solution(arr){
+  let answer = Math.min(...arr);
+  return answer;
+}
+let arr = [5, 7, 2, 3, 2, 9, 11];
+console.log(solution(arr));
