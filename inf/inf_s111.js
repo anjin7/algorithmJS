4df84824f493d0f696870c5004365c2c79b6b4c8
AnJiIn <aji0719@gmail.com>
2021-11-05 18:48:01 +0900

    인프런4

---
+++ inf/inf_s111.js
@@ -0,0 +1,12 @@
+//대문자 갯수
+
+function solution(s){         
+  let answer=0;
+  for(let x of s){
+      if(x===x.toUpperCase()) answer++;
+  }
+  return answer;
+}
+
+let str="KoreaTimeGood";
+console.log(solution(str));
