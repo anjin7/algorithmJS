aad2cde62ecbeee823f2cbd0c5675926a809346a
AnJiIn <aji0719@gmail.com>
2021-11-15 22:24:47 +0900

    인프런11

---
+++ inf/inf_s302.js
@@ -0,0 +1,9 @@
+function solution(s){
+  let answer="YES";
+  s=s.toLowerCase().replace(/[^a-z]/g, '');
+  if(s.split('').reverse().join('')!=s) return "NO";    
+  return answer;
+}
+
+let str="gooG";
+console.log(solution(str));
