9ea6d2f097b4199b89b1a8fe5e8ca3b990ea7060
_An_Jin_ <aji0719@gmail.com>
2022-04-20 14:17:10 +0900

    백준26

---
+++ bj_10926.js
@@ -0,0 +1,7 @@
+// const input = require('fs').readFileSync('/dev/stdin').toString().trim();
+
+let input = beakjoon;
+
+console.log(input + '??!');
+
+console.log(`${input}??!`);
---
+++ bj_18108.js
@@ -0,0 +1,6 @@
+// const input = require('fs').readFileSync('/dev/stdin').toString().trim();
+
+let input = 2541;
+
+let num = Number(input);
+console.log(num - 543);
