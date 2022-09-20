a8aebaa8959176c3e2bca3ab983646754e60e0a7
_An_Jin_ <aji0719@gmail.com>
2022-09-20 20:02:20 +0900

    백준 65

---
+++ bj/bj_10813.js
@@ -0,0 +1,20 @@
+// let input = require('fs').readFileSync('/dev/stdin').toString().split('\n');
+let input = [`5 4`,
+`1 2`,
+`3 4`,
+`1 4`,
+`2 2`]
+let [N, M] = input[0].split(' ').map(n => parseInt(n));
+let arr = []
+for(let i=1; i<=N; i++){
+    arr.push(i)
+};
+for(let i=1; i<=M; i++){
+    let numArr = input[i].split(' ').map(n=>parseInt(n));
+    let indexA = numArr[0] - 1;
+    let indexB = numArr[1] - 1;
+    let valueA = arr[indexA];
+    arr[indexA] = arr[indexB];
+    arr[indexB] = valueA;
+}
+console.log(arr.join(' '))
