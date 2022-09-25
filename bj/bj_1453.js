f0896036ede1539eeb2ab196c6d9f5abe7878943
_An_Jin_ <aji0719@gmail.com>
2022-09-25 20:20:23 +0900

    boj 70

---
+++ bj/bj_1453.js
@@ -0,0 +1,15 @@
+// let input = require('fs').readFileSync('/dev/stdin').toString().split('\n');
+let input = [3,
+  `1 2 3`];
+let N = parseInt(input[0]);
+let guest = input[1].split(' ').map(n=>parseInt(n));
+let arr = [];
+let cnt = 0;
+for(let i=0; i<N; i++){
+    if(arr.includes(guest[i])!==true){
+        arr.push(guest[i]);
+    }else{
+        cnt ++
+    }
+}
+console.log(cnt)
