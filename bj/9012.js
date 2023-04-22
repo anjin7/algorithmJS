01b684edc17e31d741508d24dde9e80f48a16249
_An_Jin_ <aji0719@gmail.com>
2023-04-22 23:39:50 +0900

    boj

---
+++ bj/9012.js
@@ -0,0 +1,20 @@
+// let input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
+let input = [
+    6,
+`(())())`,
+`(((()())()`,
+`(()())((()))`,
+`((()()(()))(((())))()`,
+`()()()()(()()())()`,
+`(()((())()(`,
+]
+let N = parseInt(input[0]);
+
+for(let i=1; i<=N; i++){
+    let arr = input[i].split('');
+    if(arr[0]===`)`){
+        console.log("NO")
+    }else{
+        
+    }
+}
