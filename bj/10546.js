3c0dd5e591a128c8af6c48c6033b35c16981fde2
_An_Jin_ <aji0719@gmail.com>
2023-06-18 11:55:26 +0900

    boj

---
+++ bj/10546.js
@@ -0,0 +1,21 @@
+// let input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
+let input = [
+4,
+`mislav`,
+`stanko`,
+`mislav`,
+`ana`,
+`stanko`,
+`ana`,
+`mislav`
+]
+let N = parseInt(input[0]);
+let name = input.slice(1,N+1);
+let winner = input.slice(N+1);
+for(let i=0; i<N-1; i++){
+    let idx = name.indexOf(winner[i]);
+    if(idx>-1) name.splice(idx,1)
+}
+console.log(name[0])
+
+// 시간초과
