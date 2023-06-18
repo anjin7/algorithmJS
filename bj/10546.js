e4e375f16ca70f09100ca092b8b06c1dd5454e52
_An_Jin_ <aji0719@gmail.com>
2023-06-18 11:57:21 +0900

    boj

--- bj/10546.js
+++ bj/10546.js
@@ -19,3 +19,19 @@ for(let i=0; i<N-1; i++){
 console.log(name[0])
 
 // 시간초과
+
+
+let input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
+let N = parseInt(input[0]);
+let name = input.slice(1,N+1);
+let winner = input.slice(N+1);
+let list = new Set(name);
+for(let i=0; i<(N-1); i++){
+    if(list.has(winner[i])){
+        list.delete(winner[i])
+    }
+}
+let answer = [...list];
+console.log(answer[0])
+
+// 이름의 중복이 있어서 set으로는 풀 수 없음
