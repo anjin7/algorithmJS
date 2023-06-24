4ce360059fa81565c74f7d449d67606def910467
_An_Jin_ <aji0719@gmail.com>
2023-06-24 15:36:05 +0900

    boj110

--- bj/bj_2559.js
+++ bj/bj_2559.js
@@ -3,7 +3,6 @@ let input = [`10 2`,
 `3 -2 -4 -9 0 3 7 13 8 -3`]
 let [N, K] = input[0].split(' ').map(n => parseInt(n));
 let arr = input[1].split(' ').map(n=>parseInt(n));
-let sumArr = [];
 let sum = arr.slice(0,K).reduce((acc,curnt)=>acc+curnt,0);
 let answer = sum;
 for(let i=0; i<(N-K); i++){
