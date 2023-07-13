93ae00d5c1fdba3f85aaa376b441e9f2446dc135
_An_Jin_ <aji0719@gmail.com>
2023-07-13 19:30:44 +0900

    boj

---
+++ bj/bj_28235.js
@@ -0,0 +1,10 @@
+let input = require('fs').readFileSync('/dev/stdin').toString().trim();
+if(input==="SONGDO"){
+    console.log("HIGHSCHOOL")
+}else if(input==="CODE"){
+    console.log("MASTER")
+}else if(input==="2023"){
+    console.log("0611")
+}else if(input==="ALGORITHM"){
+    console.log("CONTEST")
+}
