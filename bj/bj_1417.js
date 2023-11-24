6d5b63d9260436644e7cc319b5373b614cc12215
_An_Jin_ <aji0719@gmail.com>
2023-11-24 22:48:38 +0900

    boj 116

---
+++ bj/bj_1417.js
@@ -0,0 +1,13 @@
+let input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
+let [N, dasom, ...arr] = input.map(n=>parseInt(n));
+arr.sort((a,b)=>b-a);
+let answer = 0;
+while(true){
+    let max = Math.max(...arr);
+    if(max<dasom)break;
+    arr[0] --;
+    dasom ++;
+    answer ++;
+    arr.sort((a,b)=>b-a);
+  }
+console.log(answer)
