868e2c1c2df78b52f220b0dd92bc89578f117d29
_An_Jin_ <aji0719@gmail.com>
2023-11-24 22:58:38 +0900

    boj 116

---
+++ bj/bj_1758.js
@@ -0,0 +1,11 @@
+let input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
+let [N, ...arr] = input.map(n=>parseInt(n));
+let sum =0;
+arr.sort((a,b)=>b-a);
+for(let i=0; i<N; i++){
+    let tip = arr[i]-(i);
+    if(tip>0){
+        sum += tip
+    }
+}
+console.log(sum)
