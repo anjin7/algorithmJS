b144df9b0d9a6486b1bc229af243cd194f9f0c4d
_An_Jin_ <aji0719@gmail.com>
2023-11-26 23:09:14 +0900

    boj

---
+++ bj/bj_27889.js
@@ -0,0 +1,10 @@
+let input = require('fs').readFileSync('/dev/stdin').toString().trim();
+if(input==="NLCS"){
+    console.log("North London Collegiate School")
+}else if(input==="BHA"){
+    console.log("Branksome Hall Asia")
+}else if(input==="KIS"){
+    console.log("Korea International School")
+}else if(input==="SJA"){
+    console.log("St. Johnsbury Academy")
+}
